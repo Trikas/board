5b1c5d5d7ca78a137a02f4c69a5739ca7eb7c77d
Oleynik Vlad <oleynikprog@gmail.com>
2020-01-29 15:38:38 +0200

    Add: render users

--- route/web.php
+++ route/web.php
@@ -4,13 +4,19 @@ use App\Controllers\Dashboard;
 use \App\migration\Migration;
 use \App\seeders\SeederDb;
 use Illuminate\Database\Capsule\Manager;
+use \App\models\User;
 
 $klein = new \Klein\Klein();
-$klein->respond('GET', '/init-tables', function ($request, $response, $service) {
+$klein->respond('GET', '/init-tables', function () {
     $migration = new Migration();
     $migration->initTables();
-    $seeds =  new SeederDb();
+    $seeds = new SeederDb();
     $seeds->initSeeder();
 });
+$klein->respond('GET', '/', function ($request, $response, $service) {
+    $service->users = User::all();
+    $service->render(__DIR__.'/../views/test.php');
+});
+
 $klein->dispatch();
 
---
+++ views/test.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>board</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container-xl"><table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First name</th>
+            <th scope="col">Last name</th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?foreach($this->users as $user):?>
+        <tr>
+            <th scope="row"><?=$user->id?></th>
+            <td><?=$user->first_name?></td>
+            <td><?=$user->last_name?></td>
+            <td><a href="/csm/<?=$user->id?>">CSM board</a>&nbsp;<a href="/csmb/<?=$user->id?>">CSMB board</a></td>
+        </tr>
+        <?endforeach;?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
